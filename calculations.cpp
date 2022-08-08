9191b5787e474cbd411d9bec139f7721c36d0fc6
Chrisql <81173766+Chrisql@users.noreply.github.com>
2022-08-08 15:54:00 -0500

    Update calculations.cpp

--- calculations.cpp
+++ calculations.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
- 
-string x, choose, functionPicker, pick;
-int thing;
+
+// This is default sectoin of code that goes throughout the whole calculator
+string x, choose, functionPicker, pick, pickFuncMD, pickFuncAS;
+int endProgram;
 
 // Getting usename function
 void gettingUser() {
@@ -10,43 +11,62 @@ void gettingUser() {
 	cin >> x;
 }
 
-// The addition and subtraction function
+// The addition and subtraction function // Something else is wrong with this function I think 
 void addNSub() {
-		int addition, subtraction, add1, add2, sub1, sub2;
+	int addition, subtraction, add1, add2, sub1, sub2;
+beginningAS:
 
-		// Checking to see if the user wants to add or subtract
-		cout << x << " would you like to add or subtract?: " << endl;
-		cin >> choose;
+	// Checking to see if the user wants to add or subtract
+	cout << x << " would you like to add or subtract?: " << endl;
+	cin >> choose;
 
-		if (choose == "add") {
+	if (choose == "add") {
 
-			cout << "Enter in the first number to add: " << endl;
-			cin >> add1;
+		cout << "Enter in the first number to add: " << endl;
+		cin >> add1;
 
-			cout << "Enter the second number to add: " << endl;
-			cin >> add2;
+		cout << "Enter the second number to add: " << endl;
+		cin >> add2;
 
-			cout << "Your result is: " << add1 + add2 << endl;
+		cout << "Your result is: " << add1 + add2 << endl;
+	}
+
+	else if (choose == "subtract") {
+
+		cout << "Enter the first number to subtract: " << endl;
+		cin >> sub1;
+
+		cout << "Enter the second number to subtract: " << endl;
+		cin >> sub2;
+
+		cout << "Your result is: " << sub1 - sub2 << endl;
+	}
+	else if (choose != "add" || "subtract") {
+		cout << "Incorrrect input " << x << " do you want to try again?(yes or no): " << endl;
+		cin >> pickFuncAS;
+
+		if (pickFuncAS == "yes") { // nested if statement
+			goto beginningAS;
 		}
-
-		else if (choose == "subtract") {
-
-			cout << "Enter the first number to subtract: " << endl;
-			cin >> sub1;
-
-			cout << "Enter the second number to subtract: " << endl;
-			cin >> sub2;
-
-			cout << "Your result is: " << sub1 - sub2 << endl;
+		else if (pickFuncAS == "no") {
+			endProgram++;
 		}
-		else if (choose != "add" || "subtract") {
-			cout << "Incorrect input try again." << endl;
+		else if (pickFuncAS != "yes" || "no") { // If the user user input is still not yes or no does this below
+			cout << "Incorrect input " << x << " try again and choose either YES or NO: " << endl;
+			if (pickFuncAS == "yes") {
+				goto beginningAS;
+			}
+			else if (pickFuncAS == "no") {
+				endProgram++;
+			}
 		}
 	}
+}
 
 // The multiplication and division function
 void multiplyNDivision() {
 	double multi1, multi2, div1, div2;
+beginningMD:
 
 	cout << x << " would you like to multiply or divide?" << endl;
 	cin >> choose;
@@ -69,32 +89,78 @@ void multiplyNDivision() {
 
 		cout << "Your result is: " << div1 / div2 << endl;
 	}
+	else if (choose != "divide" || "multiply") {
+		cout << "Incorrrect input " << x << " do you want to try again?(yes or no): " << endl;
+		cin >> pickFuncMD;
+
+		if (pickFuncMD == "yes") { // nested if statement
+			goto beginningMD;
+		}
+		else if (pickFuncMD == "no") {
+			endProgram++;
+		}
+		else if (pickFuncMD != "yes" || "no") { // If the user user input is still not yes or no does this below
+			cout << "Incorrect input " << x << " try again and choose either YES or NO: " << endl;
+			if (pickFuncMD == "yes") {
+				goto beginningMD;
+			}
+			else if (pickFuncMD == "no") {
+				endProgram++;
+			}
+		}
+	}
 }
 
 void runProgram() {
+	int questionAsker = 0;
+beginningProgram:
+
 	cout << x << " would you like to use addition/subtraction or multiplication/division(as or md)? ---> ";
 	cin >> functionPicker;
 
 	if (functionPicker == "as") {
 		addNSub();
+		questionAsker++;
 	}
 	else if (functionPicker == "md") {
 		multiplyNDivision();
-	}
-	cout << x << " do you want to try again?(yes/no): " << endl;
-	cin >> pick;
+		questionAsker++;
 
-	if (pick == "no"){
-		thing++;
+	}
+	else if (functionPicker != "as" || "md") {
+		cout << "Incorrect input " << x << " do you want to try again?(yes/no): " << endl;
+		cin >> pick;
+
+		if (pick == "yes") { // Nested if statement 
+			goto beginningProgram;
+		}
+		else if (pick == "no") {
+			endProgram++;
+		}
+		else if (pick != "yes" || "no") { // if user input is still wrong program ends itself
+			cout << "Your input is still incorrect " << x << " program ended..." << endl;
+			endProgram++;
+		}
+	}
+	else if (functionPicker != "yes" || "no") {
+		endProgram++;
+	}
+	if (questionAsker == 1) {
+		cout << "Do you want to try again?(yes or no): " << endl;
+		cin >> pick;
+		questionAsker--;
+	}
+	if (pick == "no") {
+		endProgram++;
 	}
 }
 
-	// Calling the function below
-	int main() {
-		gettingUser();
+// Calling all the functions above
+int main() {
+	gettingUser();
 
-		while (thing == 0) {
-			runProgram();
-		}
-		cout << "Calculations are finished " << x << "!";
+	while (endProgram == 0) {
+		runProgram();
+	}
+	cout << "Calculations are finished " << x << "!";
 }
